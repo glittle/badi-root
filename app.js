a178cb20fb67d0da78e6756745636cbe3e6d0247
Glen Little <glen.little@gmail.com>
2017-11-05 17:02:35 -0500

    remove cmx path

--- app.js
+++ app.js
@@ -161,7 +161,7 @@ app.get('/abc', function(req, res) {
 
 app.use('/images', express.static('../Badi-Images'))
 app.use('/scripts', express.static('../scripts'))
-app.use('/cmxtrial', express.static('C:\\Dev\\codementorx\\trial\\server\\dist', { index: 'index.html' }))
+    // app.use('/cmxtrial', express.static('C:\\Dev\\codementorx\\trial\\server\\dist', { index: 'index.html' }))
 
 // app.get('/app1', function (req, res) {
 //   console.log(`Sending app file to ${req.connection.remoteAddress}`);
